b40c0c45a996a05655b8423987578f7c7134e0a2
Samuel Lyon <sblyon@me.com>
2016-03-03 15:28:02 -0800

    added os environ bit

--- app.py
+++ app.py
@@ -320,9 +320,5 @@ def addUpdateLike():
 
 if __name__ == "__main__":
 	# Bind to PORT if defined, otherwise default to 5000.
-    if 'PORT' in globals():
-    	_port = PORT
-    else:
-    	_port = 5000
-
-    app.run(host='0.0.0.0', port=_port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)

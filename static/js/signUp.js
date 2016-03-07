ead936f13fcb060b011ba4000a139700b28a0722
Samuel Lyon <sblyon@me.com>
2016-03-06 21:15:55 -0800

    various type edits

--- static/js/signUp.js
+++ static/js/signUp.js
@@ -7,12 +7,10 @@ $(function(){
             type: 'POST',
             success: function(response){
                 console.log(response);
-                console.log(typeof response);
                 var text = new String(response);
                 
                 if (text.indexOf("successfully") > -1) {
                     alert("Annnd... you're in!");
-                    // console.log("poop!");
                 window.location.href = "/showSignin";
                 }
             },

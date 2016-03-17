73679e267ba31f79343c62ca71c5565fc559d7ac
Samuel B Lyon <Sam@Samuels-MacBook-Air.local>
2016-03-17 16:40:21 -0700

    added comments, changed formatting

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ app.secret_key = 'nope'
 #sendgrid configuration
 # sg = sendgrid.SendGridClient(os.environ['SENDGRID_USERNAME'],os.environ['SENDGRID_PASSWORD'])
 
+#local DELETE BEFORE PUSH
+sg = sendgrid.SendGridClient('app48212412@heroku.com','krjz3lrj9559')
+
+
 #MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'balloon'
@@ -150,14 +154,14 @@ def addWish():
 				conn.commit()
 
 				# Notify admin of new post
-				# params = (_username,_title,_description)
-				# message = sendgrid.Mail()
-				# message.add_to('sblyon@me.com')
-				# message.set_from('twg! <hi.from.twg@gmail.com>')
-				# message.set_subject('New Post by %s' % _username)
-				# message.set_text('%s is waiting %s for %s' % params)
+				params = (_username,_title,_description)
+				message = sendgrid.Mail()
+				message.add_to('sblyon@me.com')
+				message.set_from('twg! <hi.from.twg@gmail.com>')
+				message.set_subject('New Post by %s' % _username)
+				message.set_text('%s is waiting %s for %s' % params)
 
-				# msg = sg.send(message)
+				msg = sg.send(message)
 
 				# Redirect to home
 				return redirect('/showDashboard')
@@ -189,57 +193,77 @@ def addComment():
 			cursor.execute(sql,params)
 			data = cursor.fetchall()
 
-            # cursor.callproc('sp_updateWait',(_title,_description,_wait_id,_user))
-            # data = cursor.fetchall()
-			# data = []
-
 			if len(data) is 0:
 				conn.commit()
+
+				# Get commenter username, wait poster email
+				sql = ("SELECT u.user_name as 'poster' FROM tbl_user as u JOIN tbl_comment as c on c.poster_id = u.user_id WHERE wait_id = %s")
+				params = (_wait_id,)
+				cursor.execute(sql, params)
+				result = cursor.fetchall()
+				_poster = result[0][0]
+
+				sql = ("SELECT u.user_name as 'waiter', u.user_username as 'waiter_email' FROM tbl_user as u JOIN tbl_wait as w ON w.wait_user_id = u.user_id WHERE wait_id = %s" % _wait_id)
+				cursor.execute(sql)
+				result = cursor.fetchall()
+				_waiter = result[0][0]
+				_waiter_email = result[0][1]
+
+				#Notify admin of new comment
+				message = sendgrid.Mail()
+				message.add_to('sblyon@me.com')
+				message.set_from('twg! <hi.from.twg@gmail.com>')
+				params = (_poster,_waiter)
+				message.set_subject('New comment by %s on %s\'s post' % params)
+				message.set_text(_poster + ": " + _comment)
+
+				msg = sg.send(message)
+
+				# send like email
+				params = (_poster,_poster,_comment)
+				message = sendgrid.Mail()
+				message.add_to(_waiter_email)
+				message.set_from('twg! <hi.from.twg@gmail.com>')
+				message.set_subject('New comment from %s!' % _poster)
+				message.set_html('<p>Hey, %s commented on your post!<br> %s was all like, \"%s\"<br><br>Post and comment are at twg-twg.herokuapp.com &#x1F604; <br><br><br><i>(Reply "Very hermit amaze quiet" to stop being told when people comment on your posts, or just if you feel like it.)</i></p>' % params)
+
+				msg = sg.send(message)	
+
 				return json.dumps({'status':'OK'})
 			else:
 				return json.dumps({'status':'ERROR'})
 	except Exception as e:
-		return str(e)
-		# return json.dumps({'status':'Unauthorized access'})
-	# finally:
-	# 	if session.get('user'):
-	# 		return "hi"
-		# cursor.close()
-		# conn.close()
+		return str(e)			
 
+@app.route('/getCommentsByWaitId',methods=['POST'])
+def getCommentsByWaitId():
+	try:
+		if session.get('user'):
+			_user = session.get('user')
+			_wait_id = request.form['id']			# _wait_id = 30
 
-			# Get the username of the poster
-			# cursor.execute('SELECT user_name FROM tbl_user WHERE user_id = %s',(_user,))
-			# result = cursor.fetchall()
-			# _username = result[0][0]
-			
+			con = mysql.connect()
+			cursor = con.cursor()
+			sql = ("SELECT comment_id, comment_text, user_name, (TIMESTAMPDIFF(MINUTE, NOW(), tbl_comment.comment_date) * -1) AS timeElapsed FROM tbl_comment JOIN tbl_user on tbl_comment.poster_id = tbl_user.user_id WHERE wait_id = %s")
+			params = (_wait_id,)
+			cursor.execute(sql, params)
+			comments = cursor.fetchall()
 
-	# 		if len(data) is 0:
-	# 			conn.commit()
+			comments_dict = []
+			for comment in comments:
+				comment_dict = {
+					'Id': comment[0],
+					'Comment': comment[1],
+					'Poster': comment[2],
+					'TimeElapsed': comment[3]
+					}
+				comments_dict.append(comment_dict)
 
-	# 			# Notify admin of new post
-	# 			# params = (_username,_title,_description)
-	# 			# message = sendgrid.Mail()
-	# 			# message.add_to('sblyon@me.com')
-	# 			# message.set_from('twg! <hi.from.twg@gmail.com>')
-	# 			# message.set_subject('New Post by %s' % _username)
-	# 			# message.set_text('%s is waiting %s for %s' % params)
-
-	# 			# msg = sg.send(message)
-
-	# 			# Redirect to home
-	# 			return redirect('/showDashboard')
-	# 		else:
-	# 			return render_template('error.html',error = 'An error occurred!')
-	# 			#return json.dumps({'error':str(data[0])})
-	# 	else:
-	# 		return render_template('error.html',error = 'Unauthorized Access')
-	# except Exception as e:
-	# 	return render_template('error.html',error = str(e))
-
-	# finally:
-	# 	cursor.close()
-	# 	conn.close()
+			return json.dumps(comments_dict)
+		else:
+			return render_template('error.html', error = "Unauthorized Access")
+	except Exception as e:
+		return render_template('error.html', error = str(e))
 
 @app.route('/getWait',methods=['GET'])
 def getWait():
@@ -305,17 +329,24 @@ def getAllWaits():
 			wait_data = cursor.fetchall()
 
 			# get dict of haslikeds by wait id
-			cursor.execute("select tbl_wait.wait_id, tbl_likes.wait_like FROM tbl_wait JOIN tbl_likes ON tbl_wait.wait_id = tbl_likes.wait_id WHERE user_id = %s group by wait_id",(_user,))
+			cursor.execute("SELECT tbl_wait.wait_id, tbl_likes.wait_like FROM tbl_wait JOIN tbl_likes ON tbl_wait.wait_id = tbl_likes.wait_id WHERE user_id = %s group by wait_id",(_user,))
 			has_liked_data = cursor.fetchall()
+
+			#get dict of comment counts by wait id
+			cursor.execute("SELECT wait_id, count(comment_text) FROM tbl_comment GROUP BY wait_id")
+			comment_counts = cursor.fetchall()
+			# return comment_counts
 			
-			# Convert nested lists to dictionary
+			# Convert nested lists to dictionaries
 			hasLikeds = {}
 			for wait in has_liked_data:
 				hasLikeds[str(wait[0])] = wait[1]
 
-			# return json.dumps(hasLikeds)
+			comment_counts_dict = {}
+			for comment in comment_counts:
+				comment_counts_dict[str(comment[0])] = comment[1]
 			
-			# Get hasliked by wait id
+			# Compose dic of waits. Get hasliked, comment count by wait id
 			
 			waits_dict = []
 			for row in wait_data:
@@ -328,7 +359,8 @@ def getAllWaits():
 					'Like': str(row[3]),
 					'HasLiked': str(hasLikeds[_id]) if _id in hasLikeds.keys() else 0,
 					'Poster': row[4],
-					'TimeElapsed': row[5]
+					'TimeElapsed': row[5],
+					'CommentsCount': str(comment_counts_dict[_id]) if _id in comment_counts_dict.keys() else 0
 					}
 				waits_dict.append(wait_dict)
 
@@ -351,8 +383,6 @@ def updateWish():
             _title = request.form['title']
             _description = request.form['description']
             _wait_id = request.form['id']
-
-            
 
             conn = mysql.connect()
             cursor = conn.cursor()
@@ -415,7 +445,6 @@ def addUpdateLike():
 
 				conn = mysql.connect()
 				cursor = conn.cursor()
-				#cursor.callproc('sp_getLikeStatus',(_waitId,_user,))
 				
 				sql = ("select CAST(sum(wait_like) as char), CAST(exists(select 1 from tbl_likes where wait_id = %s and user_id = %s and wait_like = 1) as char) as hasLiked from tbl_likes where wait_id = %s")
 				params = (_waitId, _user, _waitId)
@@ -424,34 +453,34 @@ def addUpdateLike():
 				result = cursor.fetchall()
 
 				# Send Like Email if Liked
-				# if result[0][1] == "1":
-				# 	# Get some params to send a like email.
-				# 	# Get post title, desc, poster email, username
-				# 	sql = ('select w.wait_title, w.wait_description, u.user_name, u.user_username as poster_email from tbl_user as u JOIN tbl_wait as w ON w.wait_user_id = u.user_id WHERE w.wait_id = %s')
-				# 	param = (_waitId,)
-				# 	cursor.execute(sql,param)
-				# 	poster_data = cursor.fetchall()
+				if result[0][1] == "1":
+					# Get some params to send a like email.
+					# Get post title, desc, poster email, username
+					sql = ('select w.wait_title, w.wait_description, u.user_name, u.user_username as poster_email from tbl_user as u JOIN tbl_wait as w ON w.wait_user_id = u.user_id WHERE w.wait_id = %s')
+					param = (_waitId,)
+					cursor.execute(sql,param)
+					poster_data = cursor.fetchall()
 					
-				# 	_title = poster_data[0][0]
-				# 	_description = poster_data[0][1]
-				# 	poster_username = poster_data[0][2]
-				# 	poster_email = poster_data[0][3]
+					_title = poster_data[0][0]
+					_description = poster_data[0][1]
+					poster_username = poster_data[0][2]
+					poster_email = poster_data[0][3]
 
-				# 	# Get liker username
-				# 	sql = ('select u.user_name from tbl_user as u where u.user_id = %s')
-				# 	param = (_user,)
-				# 	cursor.execute(sql,param)
-				# 	liker_data = cursor.fetchall()
-				# 	liker_username = liker_data[0][0]
-				# 	# send like email
-				# 	params = (liker_username,poster_username,_title,_description)
-				# 	message = sendgrid.Mail()
-				# 	message.add_to(poster_email)
-				# 	message.set_from('twg! <hi.from.twg@gmail.com>')
-				# 	message.set_subject('%s liked your post!' % liker_username)
-				# 	message.set_html('<p>Hey, %s liked your post!<br>%s is waiting %s for %s<br>Want to see your post? Head to twg-twg.herokuapp.com &#x1F604; <br><br><i>(Reply "Fuck Off!" to stop being told when people like your posts, or just if you feel like it.)</i></p>' % params)
+					# Get liker username
+					sql = ('select u.user_name from tbl_user as u where u.user_id = %s')
+					param = (_user,)
+					cursor.execute(sql,param)
+					liker_data = cursor.fetchall()
+					liker_username = liker_data[0][0]
+					# send like email
+					params = (liker_username,poster_username,_title,_description)
+					message = sendgrid.Mail()
+					message.add_to(poster_email)
+					message.set_from('twg! <hi.from.twg@gmail.com>')
+					message.set_subject('%s liked your post!' % liker_username)
+					message.set_html('<p>Hey, %s liked your post!<br>%s is waiting %s for %s<br>Want to see your post? Head to twg-twg.herokuapp.com &#x1F604; <br><br><i>(Reply "Fuck Off!" to stop being told when people like your posts, or just if you feel like it.)</i></p>' % params)
 
-				# 	msg = sg.send(message)				
+					msg = sg.send(message)				
 					
 				return json.dumps({'status':'OK','total':result[0][0],'likeStatus':result[0][1]})
 			else:
